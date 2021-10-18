720528f0e5aa750bdc9cf22a9d1a29c954186f8c
Allan <allan.miret@gmail.com>
2021-10-18 20:29:32 +0200

    ajout de commentaires

--- src/test/java/fr/dampierre/AppTest.java
+++ src/test/java/fr/dampierre/AppTest.java
@@ -38,28 +38,49 @@ class AppTest {
     }
 
     @Test
+
+    // Test sur la fonction retourneUtilisateurs lorsqu'il y a 2 clients avec le
+    // même nom
     void conditionEstUser1_retourneUtilisateurs_DevraitRetourner2() {
 
         App app = new App();
 
+        // On demande à la liste de retourner l'utilisateur user1
         List<Client> clients = app.retourneUtilisateurs("user1");
 
+        // On recupère la taille de la liste clients
         int actualInt = clients.size();
 
+        // On s'attend à ce que la liste ne soit pas nulle
         assertNotNull(clients);
+
+        // Le nombre de clients attendu est de 2, on compare et si le résultat est
+        // différent du nombre attendu
+        // on affiche le message "Il devrait y avoir 2 clients"
         assertEquals(2, actualInt, "Il devrait y avoir 2 clients");
     }
 
     @Test
-    void conditionEstInjection_retourneUtilisateurs_DevraitRetournerPlusieursClients() {
+
+    // On teste la fonction retourneUtilisateurs lorsque l'on fait une injection SQL
+    void conditionEstInjection_retourneUtilisateurs_DevraitRetournerTousLesClients() {
 
         App app = new App();
 
+        // On créé la liste clients et on lui demande de retourner l'utilisateur
+        // Geoffrey
+        // ou une condition qui est toujours vraie avec deux caractères qu'on met entre
+        // simple guillemets sauf après le dernier
         List<Client> clients = app.retourneUtilisateurs("Geoffrey' OR '1' = '1");
 
+        // On recupère la taille de la liste clients dans un int
         int actualInt = clients.size();
 
+        // On s'attend à ce que la liste ne soit pas nulle
         assertNotNull(clients);
+        // Le nombre attendu est 5 (car nombre total de clients dans ma BDD), on le
+        // compare avec le nombre obtenu
+        // Si erreur, affiche "Il devrait y avoir 5 clients"
         assertEquals(5, actualInt, "Il devrait y avoir 5 clients");
 
     }
